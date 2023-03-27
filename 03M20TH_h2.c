b63aa6b7bf4792be098f9eaaa739a39df70afebc
jeonggyu hwang <h5638880@naver.com>
2023-03-27 15:47:08 +0900

    -

--- 03M20TH_h2.c
+++ 03M20TH_h2.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
 int main() {
     // 적당히 큰 수 정하기 시작
     int random;
@@ -15,7 +22,7 @@ int main() {
     while (count != 2) {
         plag = 0;
         random = random + 1;
-        for (int i = 1; i < random; ++i) {
+        for (int i = 2; i < random; ++i) {
             if (random % i == 0) {
                 plag = plag + 1;
             }
@@ -32,23 +39,29 @@ int main() {
     printf("\nfirst_prime / second_prime : %d / %d",first_prime, second_prime);
     // 적탕히 큰 수 이상의 소수 찾기 끝
 
+    // 출력
     printf("\nN (first_prime * second_prime) : %d", first_prime * second_prime);
     printf("\nfai(N) (first_prime * second_prime) : %d", (first_prime - 1) * (second_prime - 1));
 
-    /*
     // fai(N)보다 작고 서로소인 정수중 가장 작은 정수 찾기 시작
-    int number = 2;
-    count = 0;
-    while (count == 0) {
-        plag = 0;
-        for (int i = 1; i < number; ++i) {
-            if (number % i == 0) {
-                plag = plag + 1;
-            }
-        }
-        if (plag == 0) {
-            printf("%d",number);
+    int dis = 2;
+    while (gcd(((first_prime - 1) * (second_prime - 1)), dis) != 1) {
+        dis = dis + 1;
+    }
+    printf("\ne : %d", dis);
+    // fai(N)보다 작고 서로소인 정수중 가장 작은 정수 찾기 끝
+
+    // d * e 를 fai(N) 로 나누었을때 나머지가 1인 임의의 정수 찾기 시작
+    plag = 0, count = 0;
+    while (plag != 1) {
+        count = count + 1;
+        if ((first_prime - 1) * (second_prime - 1) % (count * dis) == 1) {
+            plag = plag + 1;
         }
     }
-     */
+    // d * e 를 fai(N) 로 나누었을때 나머지가 1인 임의의 정수 찾기 끝
+
+    // 출력
+    printf("\n\n공개키: <%d, %d>", first_prime * second_prime, dis);
+    printf("\n비밀키: <%d, %d>", first_prime * second_prime, count);
 }
